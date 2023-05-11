8b9a042f03f38ba4c7247627bd8759008aeb3fc9
Hanieh Sadri <haniehsadri77@gmail.com>
2023-05-11 02:05:01 -0700

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
     }
+
+
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+
+        int buy = 0;
+        int sell = 1;
+        int profit = 0;
+        while (sell < n) {
+            if (prices[sell] > prices[buy]) {
+                profit += prices[sell] - prices[buy];
+
+            }
+
+            sell++;
+            buy++;
+
+        }
+
+        return profit;
+    }
+
+
 }
