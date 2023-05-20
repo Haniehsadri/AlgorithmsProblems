48dedacac83a63e7aa24f69267638b54f81b09c6
Hanieh Sadri <haniehsadri77@gmail.com>
2023-05-20 02:55:36 -0700

    ImpelementFunctionToRetunrThreeElementsInArrayThatSumISZERO

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,10 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -26,6 +31,110 @@ public class Main {
 
         return profit;
     }
+
+
+    /**
+     * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+     * such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+     *
+     * @param nums the integer array
+     * @return a list of triplets that satisfy the given conditions
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return Collections.emptyList();
+        }
+
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; // Skip duplicate values for the first element
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+            int target = -nums[i];
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++; // Skip duplicate values for the second element
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--; // Skip duplicate values for the third element
+                    }
+
+                    left++;
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+
+    /**
+     * Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+     * such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+     *
+     * @param nums the integer array
+     * @return a list of triplets that satisfy the given conditions
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums == null || nums.length < 3) {
+            return Collections.emptyList();
+        }
+
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue; // Skip duplicate values for the first element
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+            int target = -nums[i];
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+
+                if (sum < target) {
+                    left++;
+                } else if (sum > target) {
+                    right--;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++; // Skip duplicate values for the second element
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--; // Skip duplicate values for the third element
+                    }
+
+                    left++;
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+
 
 
 }
