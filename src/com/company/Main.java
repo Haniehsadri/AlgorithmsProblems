8a864ae0eb3a01be5595bda794484c3a3f8382e4
Hanieh Sadri <haniehsadri77@gmail.com>
2023-05-23 23:44:46 -0700

    reverse a linked list

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -227,4 +227,25 @@ public class Main {
     }
 
 
+    /**
+     * Reverses a linked list.
+     *
+     * @param head The head node of the linked list.
+     * @return The new head node of the reversed linked list.
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+
+        while (current != null) {
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+        return prev;
+    }
+
+
 }
